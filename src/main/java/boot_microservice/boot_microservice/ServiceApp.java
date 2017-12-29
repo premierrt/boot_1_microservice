5b7496dc172c15c6d87accc1c02436bfc81d1254
dom_linux <dom_linux@ddddd.xy>
2017-12-29 23:28:48 +0100

    properties poprawione

---
+++ src/main/java/boot_microservice/boot_microservice/ServiceApp.java
@@ -0,0 +1,16 @@
+package boot_microservice.boot_microservice;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by roshanalwis on 9/30/17.
+ */
+
+@SpringBootApplication
+public class ServiceApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceApp.class, args);
+    }
+}
---
+++ src/main/java/boot_microservice/boot_microservice/ServiceController.java
@@ -0,0 +1,19 @@
+package boot_microservice.boot_microservice;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by roshanalwis on 9/30/17.
+ */
+
+@RestController
+public class ServiceController {
+    @RequestMapping(value = "/heartBeat", method = RequestMethod.GET)
+    public ResponseEntity<String> heartBeat(){
+        return new ResponseEntity<String>("ACK", HttpStatus.OK);
+    }
+}
